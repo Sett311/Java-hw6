7c7f8990a09e8a08fae557b0b1540e4bf9735315
Sett311 <2004sett@gmal.com>
2023-07-07 14:23:03 +0300

    get more test

--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -7,7 +7,11 @@ public class SQRServiceTest {
     @ParameterizedTest
     @CsvSource({
             "200,300,3",
-            "100,380,10"
+            "100,380,10",
+            "9700,100000,1",
+            "9900,10000,0",
+            "0,99,0",
+            "1,100,1"
     })
     public void gottaChekTheSelectionRules(int rangeMin, int rangeMax, int expected) {
         SQRService service = new SQRService();
